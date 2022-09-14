ea87c8fcf079d1d1e72ddcdd14fee15962d1089a
Antonio Makary <Antonio.k.makary@std.balamand.edu.lb>
2022-09-14 21:24:39 +0300

    make it betterl

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ app.use((req, res, next) => {
   next()
 })
 
+app.get('/', (req, res) => { res.send('Hello from Express!')
+
+
 // routes
 app.use('/api/products', productRoutes)
 app.use('/api/orders', orderRoutes)
