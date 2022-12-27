d964c2f55d06dcdc94b80ea87cc5fb074991c204
Antonio Makary <Antonio.k.makary@std.balamand.edu.lb>
2022-12-27 13:06:01 +0200

    make it better

--- server.js
+++ server.js
@@ -19,14 +19,14 @@ app.use((req, res, next) => {
   next()
 })
 
-app.use(cors());
+//app.use(cors());
 
 // routes
 app.use('/api/products', productRoutes)
 app.use('/api/orders', orderRoutes)
 
 
-app.get('/', (req, res) => { res.send('Hello from Express!')});
+//app.get('/', (req, res) => { res.send('Hello from Express!')});
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
